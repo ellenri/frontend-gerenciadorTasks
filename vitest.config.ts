c3d85b740362a21beb9c46de16b7583c4cff450c
ellenri <ellenribeiro15@hotmail.com>
2026-09-02 11:10:00 -0300

    fix test- vitest ignore the folder e2e of playwright

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react';
+import { defineConfig, configDefaults } from "vitest/config";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: 'jsdom',
+    environment: "jsdom",
     globals: true,
-    setupFiles: ['./src/test/setup.ts'],
-    include: ['**/*.{test,spec}.{js,jsx,ts,tsx}'],
+    setupFiles: ["./src/test/setup.ts"],
+    include: ["**/*.{test,spec}.{js,jsx,ts,tsx}"],
+    exclude: ["e2e/**", ...configDefaults.exclude],
     coverage: {
-      provider: 'v8',
-      reporter: ['text', 'json', 'html'],
+      provider: "v8",
+      reporter: ["text", "json", "html"],
     },
   },
 });
